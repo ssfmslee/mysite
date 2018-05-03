09fcccf0fb7f522343ec4710b188b1ce0b8c1a8b
Rebecca Lee <ssfmslee@24dcaea7d349>
2018-05-03 22:38:38 +0000

    Added database to configuration to the flask app.

--- config.py
+++
@@ -1,7 +0,0 @@
-class BaseConfig(object):
-    SQLALCHEMY_DATABASE_URI = (
-        'mysql://<username>:'
-        '<database password>'
-        '@<username>.mysql.pythonanywhere-services.com/'
-        '<username>$mysitedb')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
