c55e534587e53109faf8a571da008c5d66302c8b
Rebecca Lee <ssfmslee@24dcaea7d349>
2018-05-04 04:16:04 +0000

    Define the course table for the database.

---
+++ config.py
@@ -0,0 +1,7 @@
+class BaseConfig(object):
+    SQLALCHEMY_DATABASE_URI = (
+        'mysql://ssfmslee:'
+        'databasepw'
+        '@ssfmslee.mysql.pythonanywhere-services.com/'
+        'ssfmslee$mysitedb')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
